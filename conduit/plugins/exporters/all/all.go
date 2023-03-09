51c688b679677d20c1ab4518a23cd4f1ff552335
julian.weber <julian.weber@secuinfra.com>
2023-03-09 18:40:57 +0100

    add kafka exporter to all

--- conduit/plugins/exporters/all/all.go
+++ conduit/plugins/exporters/all/all.go
@@ -3,6 +3,7 @@ package all
 import (
 	// Call package wide init function
 	_ "github.com/algorand/indexer/conduit/plugins/exporters/filewriter"
+	_ "github.com/algorand/indexer/conduit/plugins/exporters/kafka"
 	_ "github.com/algorand/indexer/conduit/plugins/exporters/noop"
 	_ "github.com/algorand/indexer/conduit/plugins/exporters/postgresql"
 )

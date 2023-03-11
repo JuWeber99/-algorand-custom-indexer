36bfbd08656b0678a41436be065195ac4535cc46
julian.weber <julian.weber@secuinfra.com>
2023-03-11 23:04:59 +0100

    fix

--- conduit/plugins/exporters/kafka/kafka_confluent_exporter.go
+++ conduit/plugins/exporters/kafka/kafka_confluent_exporter.go
@@ -128,9 +128,10 @@ func (exp *kafkaExporter) Receive(exportData data.BlockData) error {
 	fmt.Println(exp.round)
 	err = exp.producer.Produce(&kafka.Message{
 		TopicPartition: kafka.TopicPartition{
-			Topic: &exp.cfg.Topic, Partition: kafka.PartitionAny, Offset: kafka.Offset(exp.round),
+			Topic: &exp.cfg.Topic, Partition: kafka.PartitionAny,
 		},
 		Value: buf.Bytes(), //here eneded the encoded
+		Key:   offset,
 	}, delivery_chan)
 
 	e := <-delivery_chan

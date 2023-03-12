370d364a1697f6e1a62b5b869bd3b2b3cd6ee320
julian.weber <julian.weber@secuinfra.com>
2023-03-12 01:49:28 +0100

    DLQ t

--- conduit/plugins/exporters/kafka/kafka_confluent_config.go
+++ conduit/plugins/exporters/kafka/kafka_confluent_config.go
@@ -15,7 +15,7 @@ type KafkaExporterConfiguration struct {
 	SecurityProtocol string `yaml:"security.protocol"`
 	SaslMechanisms   string `yaml:"sasl.mechanisms"`
 	Topic            string `yaml:"topic"`
-	DlqTopic         string `yaml:"dlq.topic,omitempty "`
+	DlqTopic         string `yaml:"dlq.name,omitempty"`
 	/* <code>max-conn</code> specifies the maximum connection number for the connection pool.<br/>
 	This means the total number of active queries that can be running concurrently can never be more than this.
 	*/
